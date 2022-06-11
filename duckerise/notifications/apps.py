b1e956b0c24e052ea2214efbcb6792ef66c30c26
mmzeynalli <mmzeynall1998@gmail.com>
2022-06-11 22:20:55 +0400

    Fixed model db name prefixes and tests

--- duckerise/notifications/apps.py
+++ duckerise/notifications/apps.py
@@ -1,6 +1,9 @@
-from django.apps import AppConfig
+from django.apps import AppConfig as BaseAppConfig
+from django.utils.translation import gettext_lazy as _
 
 
-class NotificationsConfig(AppConfig):
+class AppConfig(BaseAppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "notifications"
+    name = "duckerise.notifications"
+    label = "duckerise_notifications"
+    verbose_name = _("Duckerise Notifications")
