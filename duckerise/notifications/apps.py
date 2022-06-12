28b5d2314b51a4653a434b34bafc9352e84f4eb6
mmzeynalli <mmzeynall1998@gmail.com>
2022-06-12 14:45:27 +0400

    Minor fix

--- duckerise/notifications/apps.py
+++ duckerise/notifications/apps.py
@@ -1,8 +1,8 @@
-from django.apps import AppConfig as BaseAppConfig
+from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class AppConfig(BaseAppConfig):
+class NotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "duckerise.notifications"
     label = "duckerise_notifications"
