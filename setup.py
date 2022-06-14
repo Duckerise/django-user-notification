f13ebadc13640281cb684d22ac41cfd44d89a033
Konul Mammadova <konulmammadova@starex.az>
2022-06-14 14:48:21 +0400

    updated setup file

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     python_requires=">=3.8",
     authors="Miradil Zeynalli, Konul Mammadova and Huseyn Abduallabayli",
     long_description="Django library for managing and sending notifications to users over different mediums.",
-    install_requires=["django==3.2.13", "django-ckeditor==6.4.2", "pytest-django==4.5.2"],
+    install_requires=[
+	"django-ckeditor==6.4.2",
+      ], 
+    tests_requires=["pytest-django==4.5.2"],
 )
